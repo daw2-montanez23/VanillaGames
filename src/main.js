14d385aff3f540e7c31b0ad246deb1c68fa4b771
daw2-montanez23 <montanezrosellfrancesc@fpllefia.com>
2024-01-09 20:31:59 +0100

    modificando nuestro scaffolding #16

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {header} from './componentes/header'
+import {footer} from './componentes/footer'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Importamos la vista por defecto (que será home)
+async function cargarVista(){
+  const componente = await import('./vistas/homeVista')
+  const vista = componente.default
+   // Inyectamos la vista home
+  document.querySelector('main').innerHTML = vista.template
+}
 
-setupCounter(document.querySelector('#counter'))
+cargarVista()
+
+//Inyectamos el componente header
+document.querySelector('header').innerHTML = header.template
+
+//Inyectamos el componente footer
+document.querySelector('footer').innerHTML = footer.template
